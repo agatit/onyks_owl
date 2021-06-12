35208900082d4192a1c09a49950061b9ba196edd
BRQD <hubert@kolodziejski.eu>
2021-06-12 21:21:30 +0200

    Próby róznych metoda przeysłani obrazów

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Onyks OWL
-Rail vehicles recognition and location system
+System optycznej lokalizacji wagonów
+
+## Wymagania
+Do działania programu wymagane jest zainstalowanie:
+- Redis (działajacy na domyślnym porcie)
+- TurboJPEG (w domyślnej siceżce poszukiwań)
+
+pakiety python:
+- opencv
+- redis-python
+- pillow-simd
+- blosc
+- numpy
+- scipy
+
+## Porównanie kodeków
+					kod.	zapis	dekod.	wynik (kod+2*zapis+dekod)
+pillow-simd BMP:	0.009	0.005	0.0009	0.020
+pillow-simd TIF:	0.01	0.005	0.008
+pillow-simd PNG:	0.2 	0.002	0.025
+pillow-simd JPG:	0.009	0.001	0.01	0.021
+pillow-simd 100:	0.02	0.002	0.02	0.044
+opencv		BMP:	0.004	0.005	0.002	0.016 !
+opencv		TIF:	0.06	0.003	0.04
+opencv		PNG:	0.07	0.003	0.03
+opencv		JPG:	0.03	0.002	0.02
+TurboJPEG	def:	0.008	0.001	0.009	0.019 !
+TurboJPEG	100:	0.017	0.002	0.02	0.023
+blosc   BloscLZ:	0.007	0.005	0.003	0.020
+blosc		LZ4:	0.008	0.004	0.002	0.018 !
+blosc	   ZSTD:	0.5		0.003	0.02
+numpy/pickle   :	0.004	0.005	0.003	0.017
--- owl/stream_video.py
+++ owl/stream_video.py
@@ -4,12 +4,16 @@
 # Created on: python 3.8.7
 """Moduł obsługujący strunieni video"""
 import stream_base
+import os
 import cv2 as cv
-import io
-from PIL import Image
+import turbojpeg
+# import io
+# from PIL import Image
+# import blosc
 import numpy as np
 import time
 		
+jpeg = turbojpeg.TurboJPEG()
 
 class Producer(stream_base.Producer):
     def __init__(self, redis, stream_queue, expire_time=120, queue_limit=0, timeout=120):
@@ -18,13 +22,33 @@ class Producer(stream_base.Producer):
 
     def emit(self, image):
         start = time.time()
-        img_pil = Image.fromarray(image)
-        with io.BytesIO() as b:
-            img_pil.save(b,"JPEG")
-            b.seek(0)            
-            self.encoding_time = self.encoding_time * 0.99 + (time.time() - start) * 0.01
-            print(f"kodowanie: {self.encoding_time:.5f} zapis: {self.writing_time:.5f}")
-            super().emit(b.read())
+
+        #opencv
+        # _,img_np = cv.imencode(".BMP", image)
+        # img_bytes = img_np.tobytes()
+
+        # pillow-simd
+        # img_pil = Image.fromarray(image)
+        # with io.BytesIO() as b:
+        #     img_pil.save(b,"JPEG")
+        #     b.seek(0)    
+        #     img_bytes = b.read()    
+        
+        #turbojpeg
+        img_bytes = jpeg.encode(image, quality=80)        
+
+        # blosc lz4
+        # img_bytes = blosc.pack_array(image, cname='lz4')
+
+        # numpy/pickle
+        # with io.BytesIO() as b:
+        #     np.save(b, image, allow_pickle=True)   
+        #     b.seek(0) 
+        #     img_bytes = b.read()      
+
+        self.encoding_time = self.encoding_time * 0.99 + (time.time() - start) * 0.01
+        print(f"kodowanie: {self.encoding_time:.5f} zapis: {self.writing_time:.5f}")
+        super().emit(img_bytes)
 
 
 class Consumer(stream_base.Consumer):
@@ -35,9 +59,26 @@ class Consumer(stream_base.Consumer):
     def __next__(self):              
         img_bytes = super().__next__()
         start = time.time() 
-        b = io.BytesIO(img_bytes)
-        img_pil = Image.open(b)
-        result =  np.asarray(img_pil)
+
+        #opencv
+        # img_np = np.frombuffer(img_bytes, np.uint8)
+        # result = cv.imdecode(img_np, cv.IMREAD_COLOR)
+
+        # pillow-simd
+        # b = io.BytesIO(img_bytes)
+        # img_pil = Image.open(b)
+        # result =  np.asarray(img_pil)
+
+        # turgojpeg
+        result = jpeg.decode(img_bytes)
+
+        # blosc lz4
+        # result = blosc.unpack_array(img_bytes)
+
+        # numpy/pickle
+        # b = io.BytesIO(img_bytes)
+        # result = np.load(b, allow_pickle=True)
+
         self.decoding_time = self.decoding_time * 0.99 + (time.time() - start) * 0.01
         print(f"odkodowywanie: {self.decoding_time:.5f}")
         return result
