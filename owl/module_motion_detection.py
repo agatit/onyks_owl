77924c44f5fb64b7b8a7088ae795f92e514c7568
Pawel Maciejewski <krzysiekpawel@o2.pl>
2021-07-12 15:05:13 +0200

    module motion detection with track detection

--- owl/module_motion_detection.py
+++ owl/module_motion_detection.py
@@ -36,6 +36,19 @@ class Module(module_base.Module):
         frame_gap = self.params.get("frame_gap", 5)
         motion_state = False
         frame_tail = 0
+        track_detection0 = False
+        track_detection1 = False
+        hbf, hf, wbf, wf = 550, 90, 310, 280 #track nr.0
+        hbf2, hf2, wbf2, wf2 = 600, 90, 30, 200 #track nr.1
+        track_window = (wbf, hbf, wf, hf)
+        track_window2 = (wbf2, hbf2, wf2, hf2)
+        offset_sum = 50000
+        """
+        hbf -> height base of frame
+        hf -> height of frame
+        wbf -> width base of frame
+        wf -> width of frame
+        """
 
         for input_data in input_stream:
             begin = time.time()
@@ -47,12 +60,42 @@ class Module(module_base.Module):
                 ret, bwframe = cv2.threshold(gdframe,10,255,cv2.THRESH_BINARY)
                 pixel_count = cv2.countNonZero(bwframe)
                 motion_state =  pixel_count > pixel_treshold
+                
             else:
                 motion_state = False
                         
             #TODO: wybieranie na którym torze jest największy ruch            
-            output_task_data['railtrack'] = "0"
-
+            if len(track_window) == 4 and len(track_window2) == 4:
+                if hbf > 0 and hf > 0 and wbf > 0 and wf > 0 and hbf2 > 0 and hf2 > 0 and wbf2 > 0 and wf > 0:
+                    roi = input_data['color'][hbf:hbf + hf, wbf:wbf + wf]
+                    roi2 = input_data['color'][hbf2:hbf2 + hf2, wbf2:wbf2 + wf2]
+                    hsv_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+                    hsv_roi2 = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+                    mask = np.zeros_like(hsv_roi, dtype="uint8")
+                    mask2 = np.zeros_like(hsv_roi2, dtype="uint8")
+                    Bit_wised = cv2.bitwise_and(hsv_roi, hsv_roi, mask)
+                    Bit_wised2 = cv2.bitwise_and(hsv_roi2, hsv_roi2, mask2)
+                    sum_of_roi = np.sum(roi)
+                    sum_of_roi2 = np.sum(roi2)
+                    print("sum_of_roi:", sum_of_roi)
+                    print("sum_of_roi2:", sum_of_roi2)
+                    cv2.rectangle(input_data['color'], (wbf, hbf), (wbf+wf, hbf+hf), (255, 0, 0), 2)
+                    cv2.rectangle(input_data['color'], (wbf2, hbf2), (wbf2+wf2, hbf2+hf2), (255, 255, 0), 2)
+          
+                    if sum_of_roi  < sum_of_roi2 + offset_sum:
+                        track_detection0 = True
+                        print("Detekcja toru 0 udana")
+                    else:
+                        track_detection0 = False
+                        print("Detekcja toru 0 nieudana")
+                    
+                    if sum_of_roi2 < sum_of_roi + offset_sum:
+                        track_detection1 = True
+                        print("Detekcja toru 1 udana")
+                    else:
+                        track_detection1 = False
+                        print("Detekcja toru 1 nieudana")
+                    
             if motion_state:
                 if not output_stream:                        
                     logging.debug("Motion begin")
@@ -62,7 +105,13 @@ class Module(module_base.Module):
             if output_stream:
                 if frame_tail > 0:                
                     output_stream.emit(frame_buffer[0])
-                    frame_tail -= 1                    
+                    frame_tail -= 1    
+                    if track_detection0 == True:
+                        output_task_data['railtrack'] = "0"
+                        output_stream = self.task_emit(output_task_data)  
+                    if track_detection1 == True:
+                        output_task_data['railtrack'] = "1"
+                        output_stream = self.task_emit(output_task_data)              
                 else:
                     logging.debug("Motion end")
                     output_stream.end()
@@ -70,7 +119,7 @@ class Module(module_base.Module):
 
             frame_buffer.append(output_data)
             if len(frame_buffer) > buffer_size:                
-                frame_buffer.pop(0) # do śmieci
+                frame_buffer.pop(0)
         
         # na wypadek zakończenia strumienia wejściowego podczas ruchu - emitujemy ogon
         if output_stream:
