5614158585325a41a578ebb3a1524d7c250334b6
S-T-A-L-K-R <name.generate@gmail.com>
2021-08-09 14:09:18 +0200

    dlaczego TODO jest coraz więcej, zamiast coraz mniej?

--- gui/backend/file_read.py
+++ gui/backend/file_read.py
@@ -1,36 +1,121 @@
 import os
+import json 
+import configparser
 
 MY_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)),"../../examples")
 
 class Files():
     def __init__(self, examples_dir = MY_PATH):
         self.dir = examples_dir
-        self.examples = os.listdir(path='./examples')
-# TODO zczytanie folderów
-# TODO example w słownik{'nazwa': obiekt}
-# TODO dla każdego folderu - obiekt klasy tej poniżej
-# TODO tworzenie nowego folderu/example'a
-# TODO na później - ogarnianie plików z './owl'
+        examples = os.listdir(path=MY_PATH)
+        self.examples = {}
+        for x in examples:
+            if x != 'split':
+                self.examples[x] = Example(x)
+    def get_names(self):
+        return self.examples.keys()
+    def create_example(self, name):
+        # TODO tworzenie nowego folderu/example'a
+        pass
+    def read_modules(self):
+        # TODO na później - ogarnianie plików z './owl'
+        pass
+    def xdebug(self):
+        for x in self.examples.values():
+            x.read_logs()
+
+
+
 
 class Example():
-    def __init__(self, examples_dir = MY_PATH): # TODO nieno, może bez defaulta, ale z wywalaniem błędu jak NULL
-        self.dir = examples_dir
-# TODO zczytanie "config.json" do słownika
-# TODO zapisanie przyjętego słownika jako "config.json"
-# TODO czytanie pliku "supervisord.conf", pewnie do słownika
-# TODO zapisywanie przyjętego słownika jako "supervisord.conf"
-# TODO zczytywanie plików logów
-    # logów nigdzie specjalnie nie zapisywać
-        # otworzyć plik
-        # pobrać tekst
-        # zamknąć plik
-        # zwrócić tekst
-        # elo
-    # po ogarnięciu pliku "supervisord.conf" będzie wiadomo jakich plików się spodziewać
-        # na każdy moduł plik "*-stderr" & "*-stdout"
-        # plik "supervisord.log"
-# TODO ogarnianie pliku ".bat"/".sh"
-    # nie wiem, pewnie póki co skopiować tak jak są i tyle, taki hardcode
-# TODO odpalanie/wyłączanie procesu modułu
+    def __init__(self, example_name): # TODO nieno, może bez defaulta, ale z wywalaniem błędu jak NULL
+        # TODO przygotować wersję pod "create"
+        self.name = example_name
+        self.path = os.path.join(MY_PATH, self.name)
+        self.config_path = os.path.join(self.path, 'config.json')
+        self.supervisord = configparser.ConfigParser()  # TODO okej, parser se robię, a config otwieram i zamykam...
+                                                        # co w końcu lepsze?!?!?!
+        self.exec_path = os.path.join(self.path, 'run.bat') # TODO wersja również na linuxa
+        self.logs_path = os.path.join(self.path, 'tmp')
+
+        self.read_supervisord()
+
     def read_config(self):
+        file = open(self.config_path)
+        config = json.load(file)
+        file.close()
+        return config
+
+    def save_config(self, data):
+        # TODO zapisanie przyjętego słownika jako "config.json"
         pass
+    def read_supervisord(self):
+        self.supervisord.read(os.path.join(self.path, 'supervisord.conf'))
+        # TODO przerobić zawartość obiektu na plaintext
+
+    def save_supervisord(self, data):
+        pass
+        # TODO zapisywanie przyjętego słownika jako "supervisord.conf"
+    
+    def read_logs(self):
+        ### OGARNIANIE NAZW MODUŁÓW ###
+        modules = []
+        for item in self.supervisord.sections():
+            if item.startswith("program:"):
+                # modules.append(item.removeprefix('program:'))
+                modules.append(item[8:])
+        ### OGARNIANIE NAZW I ZAWARTOŚCI PLIKÓW LOGÓW ###
+        log_file_list = os.listdir(path=self.logs_path)
+        logs = {}
+        # TODO japierdole, jeszcze teraz numerki do SORSów domontowywać :////////////
+        # TODO odpalić każdy moduł chociaż raz
+        # TODO porobić wyjątki na wypadek braku plików
+        # TODO ej mordo, tak sobie patrzę na "camera_view"
+            # No i chyba pierdolę i wysyłam jak jest i chuj
+        # TODO jak niby wysyłać video z takiego np. "camera_view"?
+        
+
+        for mod in modules:
+            # if any(mod + '-stderr' in s for s in log_file_list):
+            #     pass
+            match_out = [s for s in log_file_list if mod + '-stdout' in s]
+            match_err = [s for s in log_file_list if mod + '-stderr' in s]
+            fmo = open(os.path.join(self.logs_path, match_out))
+            logs[mod + '-stdout'] = fmo.read()
+            fme = open(os.path.join(self.logs_path, match_err))
+            logs[mod + '-stderr'] = fme.read()
+        ### NAZWY I ZAWARTOŚCI PLIKÓW - "SINK" ###
+        for i in range(self.supervisord['program:sink']['numprocs']):
+            match_out = [s for s in log_file_list if 'sink_' + str(i) + '-stdout' in s]
+            match_err = [s for s in log_file_list if 'sink_' + str(i) + '-stderr' in s]
+            fmo = open(os.path.join(self.logs_path, match_out))
+            logs[mod + '-stdout'] = fmo.read()
+            fmo.close()
+
+            fme = open(os.path.join(self.logs_path, match_err))
+            logs[mod + '-stderr'] = fme.read()
+            fme.close()
+
+        ### PLIK SUPERVISORD.LOG ###
+        fs = open(os.path.join(self.logs_path, 'supervisord.log'))
+        logs['supervisord.log'] = fs.read()
+        fs.close()
+        print(logs)
+
+    def read_exec(self):
+        file = open(self.exec_path)
+        data = json.load(file)
+        file.close()
+        return data
+    def save_exec(self, data):
+        # TODO ogarnianie pliku ".bat"/".sh"
+            # nie wiem, pewnie póki co skopiować tak jak są i tyle, taki hardcode
+        pass
+    def run_example(self):
+        # TODO odpalanie/wyłączanie procesu modułu
+        pass
+
+if __name__ == '__main__':
+    List = Files()
+    # print(List.get_names())
+    List.xdebug()
--- gui/backend/test.py
+++ gui/backend/test.py
@@ -1,4 +1,7 @@
 import os
+import configparser
+
+MY_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)),"../../examples")
 
 # for root, dirs, files in os.walk(".", topdown=False):
 # #    for name in files:
@@ -10,4 +13,50 @@ import os
 #     print(files)
 #     print("___________________________________________")
 
-print(os.listdir(path='./examples'))
+# print(os.listdir(path='./examples'))
+
+path = os.path.join(MY_PATH, 'multiple_sources')
+file = os.path.join(path, 'supervisord.conf')
+logs_path = os.path.join(path, 'tmp')
+cp = configparser.ConfigParser()
+# print(file)
+# x = open(file)
+# print(x.read())
+config = cp.read(file)
+# print(config)
+# print(type(cp))
+# print(type(config))
+# print(cp.sections())
+sec_list = []
+for item in cp.sections():
+    print(item)
+    # if item.startswith("program:"):
+        # modules.append(item.removeprefix('program:'))
+        # print(item.removeprefix('program:'))
+        # print(item[8:])
+        # sec_list.append(item[8:])
+
+# log_file_list = os.listdir(path=logs_path)
+# ez = open(logs_path + '/' + log_file_list[1])
+# print(ez.read())
+# matching = [s for s in log_file_list if "" in s]
+# for mod in sec_list:
+#     if any(mod + '-stderr' in s for s in log_file_list):
+#         # print('xd')
+#         # print(log_file_list)
+#         print(mod + '-stderr')
+#     if any(mod + '-stdout' in s for s in log_file_list):
+#         print('dx')
+#         # print(x)
+
+# xd = []
+# for mod in sec_list:
+#     matching = [s for s in log_file_list if mod + '-stdout' in s]
+#     # print(matching)
+#     # print(len(matching))
+#     if matching:
+#         xd.append(matching[0])
+
+# print(xd)
+
+# print(cp['program:sink']['numprocs'])
