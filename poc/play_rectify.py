55f6b09c7c57546c7e85445564531eed5119294e
wrzezniczak <wojtek.sqwarku@gmail.com>
2023-08-01 11:28:26 +0200

    Refaktoryzacja play_rectify.py

--- poc/play_rectify.py
+++ poc/play_rectify.py
@@ -1,31 +1,61 @@
+import click
 
 import rectify
 import cv2
 import json
 import numpy as np
 
-with open("../samples/choiny2022/gora/gora.json") as f:
-    rectify.calc_maps(json.load(f), 1920, 1080)
 
-cap = cv2.VideoCapture('../samples/choiny2022/gora/1.mp4')
- 
-if (cap.isOpened()== False): 
-  print("Error opening video stream or file")
- 
-while(cap.isOpened()):
- 
-  ret, frame = cap.read()
-  if ret == True:
- 
-    frame = rectify.rectify(frame)
-    cv2.imshow('Frame',frame)
-    
- 
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-      break
+def show_rectified_image(image_path):
+    image = cv2.imread(image_path)
 
-  else: 
-    break
+    frame = rectify.rectify(image)
 
-cap.release()
-cv2.destroyAllWindows()
+    cv2.imshow('Frame', frame)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def show_rectified_video(video_path):
+    cap = cv2.VideoCapture(video_path)
+
+    if not cap.isOpened():
+        print("Error opening video stream or file")
+
+    while cap.isOpened():
+
+        ret, frame = cap.read()
+        if ret:
+
+            frame = rectify.rectify(frame)
+            cv2.imshow('Frame', frame)
+
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+
+        else:
+            break
+    cv2.destroyAllWindows()
+    cap.release()
+
+
+@click.command()
+@click.argument("input_source")
+@click.argument("config_json")
+@click.option('--image', "action", flag_value="image")
+@click.option('--video', "action", flag_value="video")
+def main(input_source, config_json, action):
+    actions = {
+        "image": show_rectified_image,
+        "video": show_rectified_video
+    }
+
+    with open(config_json) as f:
+        rectify.calc_maps(json.load(f), 1920, 1080)
+
+    callback = actions[action]
+    callback(input_source)
+
+
+if __name__ == '__main__':
+    main()
