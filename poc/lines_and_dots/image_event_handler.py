6c4ac77267c2d144673bccb64f96ed3b6307a335
wrzezniczak <wojtek.sqwarku@gmail.com>
2023-08-31 13:09:39 +0200

    Wyświetlanie typu linii oraz zmiana jej struktury danych

--- poc/lines_and_dots/image_event_handler.py
+++ poc/lines_and_dots/image_event_handler.py
@@ -1,59 +1,20 @@
-from dataclasses import dataclass
 
 import cv2
-import numpy as np
 
 
 class ImageEventHandler:
-    def __init__(self, image, window_name):
+    def __init__(self, image, window_name, *line_types):
         self.image = image
         self.window_name = window_name
+        self.line_types = line_types
+
+        self.current_line_type = self.line_types[0]
+        self.dots = []
         self.image_copy = self.image.copy()
 
-        self.left_click_counter = 1
-        self.line_start_end_points = []
-        self.dots = []
-
     def mouse_callback(self):
-        # TODO do struktury callbacki
         def wrapper(event, x, y, flags, param):
             if event == cv2.EVENT_LBUTTONDOWN:
-                circle_kwargs = {
-                    "img": self.image,
-                    "center": (x, y),
-                    "radius": 5,
-                    "thickness": -1,
-                    "color": (0, 0, 255)
-                }
-
-                counter = self.left_click_counter
-                if counter % 3 == 0:
-                    self.line_start_end_points = []
-                    self.reload_image()
-                elif counter % 3 == 2:
-                    start_line_point = self.line_start_end_points[0]
-                    end_line_point = (x, y)
-                    self.line_start_end_points.append(end_line_point)
-
-                    line_kwargs = {
-                        "img": self.image,
-                        "pt1": start_line_point,
-                        "pt2": end_line_point,
-                        "thickness": 5,
-                        "color": (0, 0, 255)
-                    }
-
-                    cv2.line(**line_kwargs)
-                else:
-                    start_line_point = (x, y)
-                    self.line_start_end_points.append(start_line_point)
-
-                    cv2.circle(**circle_kwargs)
-
-                self.left_click_counter = counter + 1
-                cv2.imshow(self.window_name, self.image)
-
-            if event == cv2.EVENT_RBUTTONDOWN and len(self.line_start_end_points) >= 2:
                 circle_kwargs = {
                     "img": self.image,
                     "center": (x, y),
@@ -67,11 +28,29 @@ class ImageEventHandler:
                 cv2.circle(**circle_kwargs)
                 cv2.imshow(self.window_name, self.image)
 
+            if event == cv2.EVENT_RBUTTONDOWN:
+                pass
+
         return wrapper
+
+    def save_current_dots(self):
+        self.current_line_type.lines.append(self.dots)
+        self.dots = []
+
+    def display_current_line_type(self):
+        put_text_kwargs = {
+            "img": self.image,
+            "text": self.current_line_type.type,
+            "org": (0, 25),
+            "fontFace": cv2.FONT_HERSHEY_SIMPLEX,
+            "fontScale": 1,
+            "thickness": 3,
+            "color": (0, 0, 255)
+        }
+
+        cv2.putText(**put_text_kwargs)
+        cv2.imshow(self.window_name, self.image)
 
     def reload_image(self):
         self.image = self.image_copy.copy()
-        self.line_start_end_points = []
-        self.dots = []
-
         cv2.imshow(self.window_name, self.image)
--- poc/lines_and_dots/lines_and_dots.py
+++ poc/lines_and_dots/lines_and_dots.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from enum import Enum
 
 import click
@@ -6,14 +7,24 @@ import cv2
 from image_event_handler import ImageEventHandler
 
 
-class LineAndDots:
-    line: list
-    dots: list
+@dataclass
+class LineType:
+    type: str
+    lines: list
 
 
 class PressedKey(Enum):
     escape = 27
     space = 32
+    enter = 13
+
+
+def scale_image_by_percent(image, percent):
+    width = int(image.shape[1] * percent / 100)
+    height = int(image.shape[0] * percent / 100)
+    dim = (width, height)
+
+    return cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
 
 
 @click.command()
@@ -21,19 +32,63 @@ class PressedKey(Enum):
 def main(file_path):
     image = cv2.imread(file_path)
     window_name = "image"
-    image_event_handler = ImageEventHandler(image, window_name)
+
+    resize_percent = {
+        "scale_down": 75,
+        "scale_up": 133,
+    }
+    original_image = image.copy()
+    image = scale_image_by_percent(image, resize_percent["scale_down"])
+
+    horizontal_lines = LineType("horizontal", [])
+    vertical_lines = LineType("vertical", [])
+    image_event_handler = ImageEventHandler(image, window_name, horizontal_lines, vertical_lines)
 
     cv2.imshow(window_name, image)
     cv2.setMouseCallback(window_name, image_event_handler.mouse_callback())
 
+    image_event_handler.display_current_line_type()
+
     while True:
         key = cv2.waitKey(0)
 
+        # check if window still exists
+        if not cv2.getWindowProperty(window_name, cv2.WND_PROP_VISIBLE):
+            break
+
         if key == PressedKey.space.value:
-            image_event_handler.reload_image()
+            img = original_image.copy()
+            dots = image_event_handler.dots
+
+            for dot in dots:
+                x, y = dot
+
+                width = int(x * 133 / 100)
+                height = int(y * 133 / 100)
+                dim = (width, height)
+
+                circle_kwargs = {
+                    "img": img,
+                    "center": dim,
+                    "radius": 5,
+                    "thickness": -1,
+                    "color": (0, 255, 255)
+                }
+
+                cv2.circle(**circle_kwargs)
+
+            cv2.imshow('original', img)
 
         if key == PressedKey.escape.value:
             break
+
+        if key == PressedKey.enter.value:
+            image_event_handler.save_current_dots()
+
+            line_types = image_event_handler.line_types
+            print()
+            for line_type in line_types:
+                print(f"{line_type.type}: {line_type.lines}")
 
     cv2.destroyAllWindows()
 
